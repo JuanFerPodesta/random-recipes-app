40dd6024f145b33495627a3b44c0587746755831
JuanFerPodesta <juan.fernando.podesta@gmail.com>
2023-01-25 17:32:25 -0300

    d

--- random_recipes_app/settings.py
+++ random_recipes_app/settings.py
@@ -121,11 +121,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'templates'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'templates')

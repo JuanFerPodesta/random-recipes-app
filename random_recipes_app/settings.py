f3b2851dd9f0635cc4576db76cb1f61df8f86423
JuanFerPodesta <juan.fernando.podesta@gmail.com>
2023-01-26 00:19:26 -0300

    j

--- random_recipes_app/settings.py
+++ random_recipes_app/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-tbt!ay-_28x_w=d*u9cn2uy3k_)(a7e-vof07lgx=y71y&l#kn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['random-recipes-app-production.up.railway.app/']
 
 
 # Application definition
@@ -121,14 +121,11 @@ USE_TZ = True
 STATIC_URL = 'static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static'), 
+#     os.path.join(BASE_DIR, 'static'),
 # ]
-
 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-

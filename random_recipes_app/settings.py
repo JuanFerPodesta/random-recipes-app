959d18594fa343166e52b498fe4f0a43864aeb00
JuanFerPodesta <juan.fernando.podesta@gmail.com>
2023-01-26 14:31:32 -0300

    agregando lo de middlewate

--- random_recipes_app/settings.py
+++ random_recipes_app/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'rr_app.middleware.DisableCacheMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
